ac6fac781c58724b7488b7f762eb97183abdfa03
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2025-01-24 11:37:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,14 @@ function App() {
           <a className="social-link" href="https://x.com/PinterestCoin">
             <img className="social-img" src="/images/icons/x.jpg" />
           </a>
-          <a className="social-link" href="https://t.me/PinterestCoinSol">
+          <a className="social-link" href="https://t.me/PumpPinterest">
             <img className="social-img" src="/images/icons/tg.png" />
           </a>
           <a className="social-link" href="https://jup.ag/swap/pinchange">
             <img className="social-img" src="/images/icons/jupiterswapbw.jpg" />
+          </a>
+          <a className="social-link" href="https://pump.fun/coin/pinchange">
+            <img className="social-img" src="/images/icons/pumpfunbw1.jpg" />
           </a>
         </div>
       </aside>
